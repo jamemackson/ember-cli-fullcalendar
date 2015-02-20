6ca98401fbb7079b1442c629a5417118d9d2526c
Jamie White <jamie@jgwhite.co.uk>
2015-02-20 14:46:17 +0000

    Resort to global moment to satisfy ui-scheduler

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,7 @@
 var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
-app.import('bower_components/moment/moment.js');
+app.import('bower_components/moment/moment.js', { prepend: true });
 app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 app.import('bower_components/fullcalendar/dist/fullcalendar.js');
 app.import('bower_components/fullcalendar/dist/fullcalendar.css');
